c601a24a2148c0c00d643cd48ecfd2fa287228d1
Harrison Hascoe <hascoeharrison@gmail.com>
2020-08-26 00:48:08 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -6,7 +6,7 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
-def input_to_index(new_user_input)
+def input_to_index
   new_user_input = user_input.to_i
   new_user_input = 1
   return new_user_input
